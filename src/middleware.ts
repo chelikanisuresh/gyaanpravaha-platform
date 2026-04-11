80b4c03cd37e40fa5f4bde966995538eabef49d0
Suresh Chelikani <cvvsureshbabu@gmail.com>
2026-04-11 15:37:01 +0530

    fix: allow /admin/login without auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -36,6 +36,7 @@ export async function middleware(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/admin/login') &&
     !request.nextUrl.pathname.startsWith('/auth') &&
     !request.nextUrl.pathname.startsWith('/signup') &&
     !request.nextUrl.pathname.startsWith('/forgot-password') &&
